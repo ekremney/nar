cae1b125987040e76dfbfba79d7ebee297a6cfc5
Fmakdemir <fmakdemir@gmail.com>
2014-12-18 16:22:30 +0200

    backup for demo

--- projectNar/build/generated/source/buildConfig/debug/net/narlab/projectnar/BuildConfig.java
+++ projectNar/build/generated/source/buildConfig/debug/net/narlab/projectnar/BuildConfig.java
@@ -5,7 +5,7 @@ package net.narlab.projectnar;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "net.narlab.projectnar";
+  public static final String APPLICATION_ID = "net.narlab.projectnar";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
